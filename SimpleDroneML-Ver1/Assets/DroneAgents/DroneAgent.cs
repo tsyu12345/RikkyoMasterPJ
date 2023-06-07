f58c611c682ac5d9014ea6eef89022df0f945773
tsyu12345 <syukunt@icloud.com>
2023-06-07 15:44:24 +0900

    【テスト開発】とりあえず見様見真似でAgentクラスを実装。動かないけど。

--- SimpleDroneML-Ver1/Assets/DroneAgents/DroneAgent.cs
+++ SimpleDroneML-Ver1/Assets/DroneAgents/DroneAgent.cs
@@ -8,6 +8,14 @@ using Unity.MLAgents.Actuators;
 public class DroneAgent: Agent {
 
     Rigidbody rBody;
+    public float forceMultiplier = 10;
+
+
+    public Transform target;//飛行目標
+
+    public Transform fieldPlane;//飛行範囲 
+
+    public 
 
     void Start() {
         rBody = GetComponent<Rigidbody>();
@@ -19,6 +27,17 @@ public class DroneAgent: Agent {
     */
     public override void OnEpisodeBegin() {
 
+        //targetの位置を設定（床の存在する範囲内で）
+        target.localPosition = new Vector3(Random.Range(-fieldPlane.transform.localPosition.x, fieldPlane.transform.localPosition.x),
+                                            Random.Range(0.5f, 1.5f),
+                                            Random.Range(-fieldPlane.transform.localPosition.z, fieldPlane.transform.localPosition.z));
+        //エージェントの位置を設定
+        if (this.transform.localPosition.y < 0) {
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3( 0, 0.5f, 0);
+        }
+
     }
 
     /**
@@ -26,7 +45,14 @@ public class DroneAgent: Agent {
     * エージェントが観測を行う必要があるときに呼び出されます
     */
     public override void CollectObservations(VectorSensor sensor) {
+        // Target and Agent positions
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
 
+        // Agent velocity : エージェントの移動速度
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+        sensor.AddObservation(rBody.velocity.y);
     }
 
 
@@ -35,6 +61,32 @@ public class DroneAgent: Agent {
     * エージェントが観測を行う必要があるときに呼び出されます
     */
     public override void OnActionReceived(ActionBuffers actionBuffers) {
+        // Actions, size = 3 : 
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[1]; //左/右
+        controlSignal.y = actionBuffers.ContinuousActions[2]; //上昇/下降
+        controlSignal.z = actionBuffers.ContinuousActions[0]; //前進/後退
+
+        rBody.AddForce(controlSignal * forceMultiplier);
+        //報酬の設定
+        //ターゲットに近づくと報酬を得る
+        var distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+
+        //ターゲットに近づくと報酬を得る
+        if (distanceToTarget < 1.42f) {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        //飛行範囲(x,z)の外に出ると報酬を失う
+        var inArea_X = (this.transform.localPosition.x < fieldPlane.transform.localPosition.x || this.transform.localPosition.x > -fieldPlane.transform.localPosition.x);
+        var inArea_Z = (this.transform.localPosition.z < fieldPlane.transform.localPosition.z || this.transform.localPosition.z > -fieldPlane.transform.localPosition.z);
+        
+        if(!inArea_X || !inArea_Z) {
+            SetReward(-1.0f);
+            EndEpisode();
+        }
+
+
 
     }
 
@@ -42,9 +94,32 @@ public class DroneAgent: Agent {
     /**
     * エージェントの操作テスト用
     * エージェントを手動で制御したいとき（テストなど）に使用します
+    * continuousActions[0]が前進/後退、
+    * continuousActions[1]が左/右、
+    * continuousActions[2]が上昇/下降
     */
     public override void Heuristic(in ActionBuffers actionsOut) {
+        var continuousActions = actionsOut.ContinuousActions;
 
+        continuousActions.Clear();
+
+        if(Input.GetKey(KeyCode.W)) {
+            continuousActions[0] = 1;
+        } else if(Input.GetKey(KeyCode.S)) {
+            continuousActions[0] = -1;
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            continuousActions[1] = -1;
+        } else if(Input.GetKey(KeyCode.D)) {
+            continuousActions[1] = 1;
+        }
+
+        if(Input.GetKey(KeyCode.Q)) {
+            continuousActions[2] = 1;
+        } else if(Input.GetKey(KeyCode.E)) {
+            continuousActions[2] = -1;
+        }
     }
 
 }
