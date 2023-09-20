9a0ddf214eca443b1e4dc9043b0a0e9e7342f6b3
tsyu12345 <syukut@gmail.com>
2023-09-20 20:32:52 +0900

    【開発環境】mlagents-learnコマンドの引数値をyamlファイルから読むように変更

--- learning.ps1
+++ learning.ps1
@@ -1,13 +1,37 @@
 param(
     [switch]$overwrite = $false
 )
-#runidとconfigpathをプリント
-Write-Host "--run-id: $env:RUN_ID"
-Write-Host "config yaml path: $env:CONFIG_PATH"
+
+
+# Check if the module is installed
+$module = Get-Module -ListAvailable -Name powershell-yaml
+
+# If the module is not installed, elevate permissions and install it
+if ($null -eq $module) {
+    # Elevate to admin and install the module
+    Start-Process -Verb RunAs PowerShell -ArgumentList {
+        Install-Module -Name powershell-yaml -Force -SkipPublisherCheck
+        Import-Module powershell-yaml
+    }
+} else {
+    Import-Module powershell-yaml
+}
+
+# Read the YAML file into a PowerShell object
+$config = (Get-Content -Path './learning_config.yaml' | Out-String) | ConvertFrom-Yaml
+
+# Extract the variables from the config object
+$run_id = $config.RUN_ID
+$config_path = $config.CONFIG_PATH
+
+# Print the variables
+Write-Host "--run-id: $run_id"
+Write-Host "config yaml path: $config_path"
 Write-Host "--force : $overwrite"
 
+# Execute the mlagents-learn command
 if($overwrite){
-    mlagents-learn $env:CONFIG_PATH --run-id=$env:RUN_ID --force
+    mlagents-learn $config_path --run-id=$run_id --force
 } else {
-    mlagents-learn $env:CONFIG_PATH --run-id=$env:RUN_ID
+    mlagents-learn $config_path --run-id=$run_id --resume
 }
