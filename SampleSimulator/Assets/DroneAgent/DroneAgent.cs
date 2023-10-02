d403895065191ea232e027d71ac59a8c9ad06f2a
tsyu1234 <syukunt@gmail.com>
2023-10-03 01:14:10 +0900

    【テスト開発：SampleSimulator】報酬系、パラメータの見直し。負の報酬を全撤去。中間層2➞3,ノード数256➞128へ。

--- SampleSimulator/Assets/DroneAgent/DroneAgent.cs
+++ SampleSimulator/Assets/DroneAgent/DroneAgent.cs
@@ -60,7 +60,7 @@ namespace Drone {
         void OnTriggerEnter(Collider other) {
             if(other.gameObject.tag == "obstacle") {
                 Debug.Log("[Agent] Hit Obstacle");
-                AddReward(-20.0f);
+                //AddReward(-20.0f);
                 EndEpisode();
             }
             if(other.gameObject.tag == "warehouserange") {
@@ -118,7 +118,7 @@ namespace Drone {
 
         public override void OnEpisodeBegin() {
             // ドローンの位置をDronePlatformの位置に初期化
-            Vector3 pos = new Vector3(DronePlatform.transform.localPosition.x, DronePlatform.transform.localPosition.y + 3f, DronePlatform.transform.localPosition.z);
+            Vector3 pos = new Vector3(DronePlatform.transform.localPosition.x, 10f, DronePlatform.transform.localPosition.z);
             transform.localPosition = pos;
             //ドローンの状態を初期化
             isGetSupplie = false;
@@ -301,13 +301,13 @@ namespace Drone {
                     isGetSupplie = false;
                     EndEpisode();
                 } else if(!isGetSupplie) { //物資を持っていない状態で物資を離した場合
-                    AddReward(-10.0f);
+                    //AddReward(-10.0f);
                     Debug.Log("[Agent] not get Supplie... but Agent did release");
                     isGetSupplie = false;
                     EndEpisode();
                 } else if(!isOnShelter && isGetSupplie) { //避難所の上空以外で物資を離した場合
                     Debug.Log("[Agent] Release Supplie on Field. But not on Shelter");
-                    AddReward(-10.0f);
+                    //AddReward(-10.0f);
                     isGetSupplie = false;
                     EndEpisode();
                 }
